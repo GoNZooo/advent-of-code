b2c37e7b7b032b4d19ee31343cfb32a5316a874a
Rickard Andersson <gonz@severnatazvezda.com>
2019-12-31 19:32:42 +0200

    add second solution

--- zig/year-2015/src/day06.zig
+++ zig/year-2015/src/day06.zig
@@ -11,28 +11,137 @@ const Coordinates = struct {
     y: usize,
 };
 
-fn Lights(comptime L: usize) type {
-    return [L][L]bool;
+fn Lights(comptime L: usize, comptime T: type) type {
+    return [L][L]T;
 }
 
-pub fn main() void {}
+pub fn main() anyerror!void {
+    std.debug.warn("Day 06:\n", .{});
 
-fn initialLights(comptime L: usize) Lights(L) {
-    var lights: Lights(L) = undefined;
+    var bool_lights = initialLights(1000, bool, false);
+    var lines1 = try utilities.splitIntoLines(std.heap.page_allocator, input);
+    for (lines1) |line| {
+        const command = try Command.fromLine(line);
+        switch (command) {
+            .TurnOn => |cs| turnOn(1000, &bool_lights, cs.c1, cs.c2),
+            .TurnOff => |cs| turnOff(1000, &bool_lights, cs.c1, cs.c2),
+            .Toggle => |cs| toggle(1000, &bool_lights, cs.c1, cs.c2),
+        }
+    }
+
+    const solution_1_count = countLights(1000, bool_lights);
+    std.debug.warn("\tSolution 1: {}\n", .{solution_1_count});
+
+    var u32_lights = initialLights(1000, u32, 0);
+    var lines2 = try utilities.splitIntoLines(std.heap.page_allocator, input);
+    for (lines2) |line| {
+        const command = try Command.fromLine(line);
+        switch (command) {
+            .TurnOn => |cs| raise(1000, &u32_lights, cs.c1, cs.c2),
+            .TurnOff => |cs| dim(1000, &u32_lights, cs.c1, cs.c2),
+            .Toggle => |cs| raiseTwo(1000, &u32_lights, cs.c1, cs.c2),
+        }
+    }
+    const solution_2_count = countBrightness(1000, u32_lights);
+    std.debug.warn("\tSolution 2: {}\n", .{solution_2_count});
+}
+
+fn initialLights(comptime L: usize, comptime T: type, comptime initial_value: T) Lights(L, T) {
+    var lights: Lights(L, T) = undefined;
 
     for (lights) |*row| {
-        row.* = [_]bool{false} ** L;
+        row.* = [_]T{initial_value} ** L;
     }
 
     return lights;
 }
 
-fn turnOn(comptime L: usize, lights: *Lights(L), c1: Coordinates, c2: Coordinates) void {
+fn turnOn(
+    comptime L: usize,
+    lights: *Lights(L, bool),
+    c1: Coordinates,
+    c2: Coordinates,
+) void {
     var current_y = c1.y;
     while (current_y <= c2.y) : (current_y += 1) {
         var current_x = c1.x;
         while (current_x <= c2.x) : (current_x += 1) {
             lights[current_y][current_x] = true;
+        }
+    }
+}
+
+fn turnOff(
+    comptime L: usize,
+    lights: *Lights(L, bool),
+    c1: Coordinates,
+    c2: Coordinates,
+) void {
+    var current_y = c1.y;
+    while (current_y <= c2.y) : (current_y += 1) {
+        var current_x = c1.x;
+        while (current_x <= c2.x) : (current_x += 1) {
+            lights[current_y][current_x] = false;
+        }
+    }
+}
+
+fn toggle(
+    comptime L: usize,
+    lights: *Lights(L, bool),
+    c1: Coordinates,
+    c2: Coordinates,
+) void {
+    var current_y = c1.y;
+    while (current_y <= c2.y) : (current_y += 1) {
+        var current_x = c1.x;
+        while (current_x <= c2.x) : (current_x += 1) {
+            lights[current_y][current_x] = !lights[current_y][current_x];
+        }
+    }
+}
+
+fn raise(
+    comptime L: usize,
+    lights: *Lights(L, u32),
+    c1: Coordinates,
+    c2: Coordinates,
+) void {
+    var current_y = c1.y;
+    while (current_y <= c2.y) : (current_y += 1) {
+        var current_x = c1.x;
+        while (current_x <= c2.x) : (current_x += 1) {
+            lights[current_y][current_x] += 1;
+        }
+    }
+}
+
+fn dim(
+    comptime L: usize,
+    lights: *Lights(L, u32),
+    c1: Coordinates,
+    c2: Coordinates,
+) void {
+    var current_y = c1.y;
+    while (current_y <= c2.y) : (current_y += 1) {
+        var current_x = c1.x;
+        while (current_x <= c2.x) : (current_x += 1) {
+            if (lights[current_y][current_x] > 0) lights[current_y][current_x] -= 1;
+        }
+    }
+}
+
+fn raiseTwo(
+    comptime L: usize,
+    lights: *Lights(L, u32),
+    c1: Coordinates,
+    c2: Coordinates,
+) void {
+    var current_y = c1.y;
+    while (current_y <= c2.y) : (current_y += 1) {
+        var current_x = c1.x;
+        while (current_x <= c2.x) : (current_x += 1) {
+            lights[current_y][current_x] += 2;
         }
     }
 }
@@ -53,6 +162,18 @@ fn countLights(comptime L: usize, lights: [L][L]bool) usize {
     for (lights) |row| {
         for (row) |l| {
             if (l) count += 1;
+        }
+    }
+
+    return count;
+}
+
+fn countBrightness(comptime L: usize, lights: [L][L]u32) usize {
+    var count: usize = 0;
+
+    for (lights) |row| {
+        for (row) |l| {
+            count += l;
         }
     }
 
@@ -130,21 +251,21 @@ test "input file is parsed correctly" {
 }
 
 test "`turnOn(0,0, 999,999)` returns correct result" {
-    var lights = initialLights(1000);
+    var lights = initialLights(1000, bool, false);
     turnOn(1000, &lights, Coordinates{ .x = 0, .y = 0 }, Coordinates{ .x = 999, .y = 999 });
     const count = countLights(1000, lights);
     testing.expectEqual(count, 1000000);
 }
 
 test "`turnOn(0,0, 999,0)` returns correct result" {
-    var lights = initialLights(1000);
+    var lights = initialLights(1000, bool, false);
     turnOn(1000, &lights, Coordinates{ .x = 0, .y = 0 }, Coordinates{ .x = 999, .y = 0 });
     const count = countLights(1000, lights);
     testing.expectEqual(count, 1000);
 }
 
 test "`turnOn(499,499, 500,500)` returns correct result" {
-    var lights = initialLights(1000);
+    var lights = initialLights(1000, bool, false);
     turnOn(1000, &lights, Coordinates{ .x = 499, .y = 499 }, Coordinates{ .x = 500, .y = 500 });
     const count = countLights(1000, lights);
     testing.expectEqual(count, 4);
