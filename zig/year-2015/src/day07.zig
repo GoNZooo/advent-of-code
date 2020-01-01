872f1cce37c81b7a6ca97e9a49dcbaa7220c6022
Rickard Andersson <gonz@severnatazvezda.com>
2020-01-01 20:04:30 +0200

    complete day 7

--- zig/year-2015/src/day07.zig
+++ zig/year-2015/src/day07.zig
@@ -4,10 +4,32 @@ const mem = std.mem;
 const ascii = std.ascii;
 const debug = std.debug;
 const fmt = std.fmt;
+const HashMap = std.hash_map.StringHashMap;
 
 const utilities = @import("./utilities.zig");
 
-pub fn main() void {}
+pub fn main() anyerror!void {
+    debug.warn("Day 07:\n", .{});
+    var store = Store.init(std.heap.page_allocator);
+
+    const lines = try utilities.splitIntoLines(std.heap.page_allocator, input);
+    for (lines) |line| {
+        const op = try Operation.fromLine(line);
+        try store.store(op);
+    }
+
+    const solution_1 = try store.execute("a");
+    debug.warn("\tSolution 1: {}\n", .{solution_1});
+
+    var store2 = Store.init(std.heap.page_allocator);
+    for (lines) |line| {
+        const op = try Operation.fromLine(line);
+        try store2.store(op);
+    }
+    try store2.putInCache("b", solution_1);
+    const solution_2 = try store2.execute("a");
+    debug.warn("\tSolution 2: {}\n", .{solution_2});
+}
 
 const Operand = union(enum) {
     Literal: u16,
@@ -23,6 +45,24 @@ const Operand = union(enum) {
         }
 
         return error.UnableToParseOperand;
+    }
+
+    pub fn format(
+        value: Operand,
+        comptime format_string: []const u8,
+        options: std.fmt.FormatOptions,
+        context: var,
+        comptime Errors: type,
+        output: fn (@TypeOf(context), []const u8) Errors!void,
+    ) Errors!void {
+        switch (value) {
+            .Literal => |v| {
+                try fmt.format(context, Errors, output, "{}", .{v});
+            },
+            .Identifier => |id| {
+                try fmt.format(context, Errors, output, "{}", .{id});
+            },
+        }
     }
 };
 
@@ -42,7 +82,7 @@ const Operation = struct {
 
 const ShiftData = struct {
     operand: Operand,
-    shift: u16,
+    shift: u4,
 };
 
 const Operands = struct {
@@ -62,8 +102,8 @@ const Instruction = union(enum) {
         var it = mem.tokenize(string, " ");
         const first_token = it.next().?;
 
-        if (utilities.all(u8, first_token, ascii.isDigit)) {
-            const value = try Operand.fromString(first_token);
+        if (utilities.all(u8, string, ascii.isDigit)) {
+            const value = try Operand.fromString(string);
 
             return Instruction{ .Value = value };
         } else if (mem.eql(u8, first_token, "NOT")) {
@@ -89,26 +129,193 @@ const Instruction = union(enum) {
                 else => unreachable,
             };
 
-            return Instruction{ .LSHIFT = ShiftData{ .operand = left_operand, .shift = shift } };
+            return Instruction{
+                .LSHIFT = ShiftData{ .operand = left_operand, .shift = @intCast(u4, shift) },
+            };
         } else if (mem.eql(u8, instruction_token.?, "RSHIFT")) {
             const shift = switch (right_operand) {
                 .Literal => |value| value,
                 else => unreachable,
             };
 
-            return Instruction{ .RSHIFT = ShiftData{ .operand = left_operand, .shift = shift } };
+            return Instruction{
+                .RSHIFT = ShiftData{ .operand = left_operand, .shift = @intCast(u4, shift) },
+            };
         }
 
         return error.UnableToParseInstruction;
     }
+
+    pub fn format(
+        value: Instruction,
+        comptime format_string: []const u8,
+        options: std.fmt.FormatOptions,
+        context: var,
+        comptime Errors: type,
+        output: fn (@TypeOf(context), []const u8) Errors!void,
+    ) Errors!void {
+        switch (value) {
+            .Value => |v| {
+                try fmt.format(context, Errors, output, "{}", .{v});
+            },
+            .AND => |ops| {
+                try fmt.format(context, Errors, output, "{} AND {}", .{ ops.left, ops.right });
+            },
+            .OR => |ops| {
+                try fmt.format(context, Errors, output, "{} OR {}", .{ ops.left, ops.right });
+            },
+            .LSHIFT => |shift_data| {
+                try fmt.format(
+                    context,
+                    Errors,
+                    output,
+                    "{} << {}",
+                    .{ shift_data.operand, shift_data.shift },
+                );
+            },
+            .RSHIFT => |shift_data| {
+                try fmt.format(
+                    context,
+                    Errors,
+                    output,
+                    "{} >> {}",
+                    .{ shift_data.operand, shift_data.shift },
+                );
+            },
+            .NOT => |op| {
+                try fmt.format(context, Errors, output, "NOT {}", .{op});
+            },
+        }
+    }
 };
 
-test "input parses correctly" {
-    var lines = try utilities.splitIntoLines(std.heap.page_allocator, input);
+const Store = struct {
+    values: HashMap(Instruction),
+    cache: HashMap(u16),
+
+    pub fn init(allocator: *mem.Allocator) Store {
+        return Store{
+            .values = HashMap(Instruction).init(allocator),
+            .cache = HashMap(u16).init(allocator),
+        };
+    }
+
+    pub fn store(self: *Store, operation: Operation) !void {
+        try self.values.putNoClobber(operation.destination, operation.instruction);
+    }
+
+    pub fn get(self: Store, destination: []const u8) Instruction {
+        return self.values.getValue(destination) orelse
+            @panic("Trying to get non-existant destination");
+    }
+
+    pub fn putInCache(self: *Store, destination: []const u8, value: u16) !void {
+        _ = try self.cache.put(destination, value);
+    }
+
+    pub fn execute(self: *Store, destination: []const u8) !u16 {
+        const cached_value = self.cache.getValue(destination);
+
+        if (cached_value) |value| {
+            return value;
+        }
+
+        const instruction = self.get(destination);
+        // debug.warn("{} -> {}\n", .{ instruction, destination });
+
+        return switch (instruction) {
+            .Value => |op| try self.evaluateOperand(op),
+            .AND => |ops| (try self.evaluateOperand(ops.left)) & (try self.evaluateOperand(ops.right)),
+            .OR => |ops| (try self.evaluateOperand(ops.left)) | (try self.evaluateOperand(ops.right)),
+            .NOT => |op| ~(try self.evaluateOperand(op)),
+            .LSHIFT => |shift_data| (try self.evaluateOperand(shift_data.operand)) << shift_data.shift,
+            .RSHIFT => |shift_data| (try self.evaluateOperand(shift_data.operand)) >> shift_data.shift,
+        };
+    }
+
+    pub fn evaluateOperand(self: *Store, operand: Operand) error{OutOfMemory}!u16 {
+        return switch (operand) {
+            .Literal => |x| x,
+            .Identifier => |id| ret: {
+                const cached_value = self.cache.getValue(id);
+
+                if (cached_value) |value| {
+                    // debug.warn("have cached value ({}) for '{}'\n", .{ value, id });
+                    break :ret value;
+                } else {
+                    const v = try self.execute(id);
+                    try self.cache.putNoClobber(id, v);
+                    break :ret v;
+                }
+            },
+        };
+    }
+};
+
+test "test input parses and executes" {
+    var lines = try utilities.splitIntoLines(std.heap.page_allocator, test_input);
+    var store = Store.init(std.heap.page_allocator);
 
     for (lines) |line| {
         const op = try Operation.fromLine(line);
+        try store.store(op);
     }
+
+    testing.expectEqual(store.values.count(), 8);
+
+    const d = store.execute("d");
+    const e = store.execute("e");
+    const f = store.execute("f");
+    const g = store.execute("g");
+    const h = store.execute("h");
+    const i = store.execute("i");
+    const x = store.execute("x");
+    const y = store.execute("y");
+    testing.expectEqual(d, 72);
+    testing.expectEqual(e, 507);
+    testing.expectEqual(f, 492);
+    testing.expectEqual(g, 114);
+    testing.expectEqual(h, 65412);
+    testing.expectEqual(i, 65079);
+    testing.expectEqual(x, 123);
+    testing.expectEqual(y, 456);
+}
+
+test "input executes" {
+    var lines = try utilities.splitIntoLines(std.heap.page_allocator, input);
+    var store = Store.init(std.heap.page_allocator);
+
+    for (lines) |line| {
+        const op = try Operation.fromLine(line);
+        try store.store(op);
+    }
+
+    testing.expectEqual(store.values.count(), 339);
+
+    const a = try store.execute("a");
+    testing.expectEqual(a, 3176);
+
+    var store2 = Store.init(std.heap.page_allocator);
+
+    for (lines) |line| {
+        const op = try Operation.fromLine(line);
+        try store2.store(op);
+    }
+
+    try store2.putInCache("b", a);
+    const a2 = store2.execute("a");
+    testing.expectEqual(a2, 14710);
 }
 
 const input = @embedFile("input-day07.txt");
+
+const test_input =
+    \\123 -> x
+    \\456 -> y
+    \\x AND y -> d
+    \\x OR y -> e
+    \\x LSHIFT 2 -> f
+    \\y RSHIFT 2 -> g
+    \\NOT x -> h
+    \\NOT y -> i
+;
